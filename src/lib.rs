c0e485d606a4807d2fe55a7bdcab9831dc703fd6
b3nj5m1n <b3nj4m1n@gmx.net>
2023-04-29 20:21:45 +0200

    Implement cursor
    I actually wrote this immediately after the last commit, but I forgot to
    commit back and now I'm too lazy to read through exactly what I changed

--- src/lib.rs
+++ src/lib.rs
@@ -29,38 +29,106 @@ pub struct Config {
     pub prompt_hint: String,
 }
 
+pub struct State {
+    pub cursor_position: u16,
+}
+
 pub trait Promptable
 where
     Self: Sized,
 {
     fn render_prompt(
-        state: &mut Self,
+        result: &mut Self,
         role: &Role,
         status: &Status,
         config: &Config,
         event: Option<event::KeyEvent>,
+        state: &mut State,
     ) -> Result<bool>;
-    fn prompt(initial_state: Self, role: Role, status: Status, config: Config) -> Result<Self>;
+    fn prompt(initial_result: Self, role: Role, status: Status, config: Config) -> Result<Self>;
+}
+
+fn get_input_prefix(s: String, i: u16) -> String {
+    return s.chars().take(i.into()).collect();
+}
+fn get_input_suffix(s: String, i: u16) -> String {
+    return s.chars().skip(i.into()).collect();
+}
+
+fn insert_char_into_input(s: &mut String, i: u16, c: char) {
+    let mut result = String::from("");
+    result.push_str(&get_input_prefix(s.to_string(), i));
+    result.push_str(&c.to_string());
+    result.push_str(&get_input_suffix(s.to_string(), i));
+    s.clear();
+    s.push_str(result.as_str());
+}
+
+fn remove_char_from_input(s: &mut String, i: u16) {
+    let mut result = String::from("");
+    result.push_str(&get_input_prefix(
+        s.to_string(),
+        match i.checked_sub(1) {
+            Some(x) => x,
+            None => 0,
+        },
+    ));
+    result.push_str(&get_input_suffix(s.to_string(), i));
+    s.clear();
+    s.push_str(result.as_str());
+}
+
+fn truncate(s: String, width: u16) -> String {
+    s.chars().take(width.into()).collect()
+}
+
+fn move_cursor(state: &mut State, i: i8, width: u16) {
+    if i > 0 {
+        state.cursor_position =
+            (state.cursor_position + u16::try_from(i).expect("Unreachable")).clamp(0, width);
+    } else {
+        state.cursor_position = match state
+            .cursor_position
+            .checked_sub(u16::try_from(i.abs()).expect("Unreachable"))
+        {
+            Some(x) => x,
+            None => 0,
+        }
+    }
 }
 
 impl Promptable for String {
     fn render_prompt(
-        state: &mut Self,
+        result: &mut Self,
         role: &Role,
         status: &Status,
         config: &Config,
         event: Option<event::KeyEvent>,
+        state: &mut State,
     ) -> Result<bool> {
+        let width = 30;
         match event {
             Some(event) => match event.code {
-                event::KeyCode::Char(c) => state.push(c),
-                event::KeyCode::Backspace => drop(state.pop()),
+                event::KeyCode::Char(c) => {
+                    insert_char_into_input(result, state.cursor_position, c);
+                    move_cursor(state, 1, width);
+                }
+                event::KeyCode::Backspace => {
+                    remove_char_from_input(result, state.cursor_position);
+                    move_cursor(state, -1, width);
+                }
+                event::KeyCode::Left => {
+                    move_cursor(state, -1, width);
+                }
+                event::KeyCode::Right => {
+                    move_cursor(state, 1, width);
+                }
                 event::KeyCode::Enter => return Ok(true),
                 _ => (),
             },
             None => (),
         };
-        let state_len = u16::try_from(UnicodeWidthStr::width(state.as_str()))?;
+        let result_len = u16::try_from(UnicodeWidthStr::width(result.as_str()))?;
         let prompt_len = u16::try_from(UnicodeWidthStr::width(config.prompt_text.as_str()))?;
         let hint_len = u16::try_from(UnicodeWidthStr::width(config.prompt_hint.as_str()))?;
         execute!(
@@ -84,13 +152,13 @@ impl Promptable for String {
                 Role::Completed => style::SetForegroundColor(style::Color::Green),
                 Role::Aborted => style::SetForegroundColor(style::Color::Red),
             },
-            match state.is_empty() {
-                true => style::Print(config.prompt_hint.clone()),
-                false => style::Print(state.to_string()),
+            match result.is_empty() {
+                true => style::Print(truncate(config.prompt_hint.clone(), width)),
+                false => style::Print(truncate(result.to_string(), width)),
             },
-            match state.is_empty() {
+            match result.is_empty() {
                 true => cursor::MoveToColumn(prompt_len),
-                false => cursor::MoveToColumn(prompt_len + state_len),
+                false => cursor::MoveToColumn(prompt_len + state.cursor_position),
             },
             style::ResetColor
         )?;
@@ -98,21 +166,29 @@ impl Promptable for String {
     }
 
     fn prompt(
-        init_state: Self,
+        init_result: Self,
         init_role: Role,
         init_status: Status,
         init_config: Config,
     ) -> Result<Self> {
         crossterm::terminal::enable_raw_mode()?;
-        let mut state = init_state;
+        let mut result = init_result;
         let role = init_role;
         let status = init_status;
         let config = init_config;
-        Self::render_prompt(&mut state, &role, &status, &config, None)?;
+        let mut state = State { cursor_position: 0 };
+        Self::render_prompt(&mut result, &role, &status, &config, None, &mut state)?;
         loop {
             match event::read()? {
                 event::Event::Key(key) => {
-                    if Self::render_prompt(&mut state, &role, &status, &config, Some(key))? {
+                    if Self::render_prompt(
+                        &mut result,
+                        &role,
+                        &status,
+                        &config,
+                        Some(key),
+                        &mut state,
+                    )? {
                         break;
                     }
                 }
@@ -121,7 +197,7 @@ impl Promptable for String {
         }
         crossterm::terminal::disable_raw_mode()?;
         execute!(io::stdout(), style::Print("\n"))?;
-        Ok(state)
+        Ok(result)
     }
 }
 
