7acab7bc16bf11872d3f2d3a01a7c547bc0158ac
b3nj5m1n <b3nj4m1n@gmx.net>
2023-04-30 19:42:09 +0200

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub enum Role {
 }
 
 #[non_exhaustive]
+#[derive(Debug)]
 pub enum Status {
     Neutral,
     Uncertain,
@@ -24,13 +25,25 @@ pub enum Status {
     Invalid,
 }
 
+#[derive(Debug)]
 pub struct Config {
     pub prompt_text: String,
     pub prompt_hint: String,
+    pub max_width: Option<usize>,
 }
 
-pub struct State {
-    pub cursor_position: u16,
+#[derive(Debug)]
+pub struct State<T> {
+    pub input: Option<T>,
+    pub cursor_position: usize,
+}
+
+fn get_width(config: &Config) -> Result<usize> {
+    let terminal_width = usize::from(crossterm::terminal::size()?.0) - 1;
+    Ok(match config.max_width {
+        Some(x) => std::cmp::min(x, terminal_width),
+        None => terminal_width,
+    })
 }
 
 pub trait Promptable
@@ -38,99 +51,121 @@ where
     Self: Sized,
 {
     fn render_prompt(
-        result: &mut Self,
         role: &Role,
         status: &Status,
         config: &Config,
         event: Option<event::KeyEvent>,
-        state: &mut State,
+        state: &mut State<Self>,
     ) -> Result<bool>;
-    fn prompt(initial_result: Self, role: Role, status: Status, config: Config) -> Result<Self>;
-}
-
-fn get_input_prefix(s: String, i: u16) -> String {
-    return s.chars().take(i.into()).collect();
-}
-fn get_input_suffix(s: String, i: u16) -> String {
-    return s.chars().skip(i.into()).collect();
-}
-
-fn insert_char_into_input(s: &mut String, i: u16, c: char) {
-    let mut result = String::from("");
-    result.push_str(&get_input_prefix(s.to_string(), i));
-    result.push_str(&c.to_string());
-    result.push_str(&get_input_suffix(s.to_string(), i));
-    s.clear();
-    s.push_str(result.as_str());
-}
-
-fn remove_char_from_input(s: &mut String, i: u16) {
-    let mut result = String::from("");
-    result.push_str(&get_input_prefix(
-        s.to_string(),
-        match i.checked_sub(1) {
-            Some(x) => x,
-            None => 0,
-        },
-    ));
-    result.push_str(&get_input_suffix(s.to_string(), i));
-    s.clear();
-    s.push_str(result.as_str());
-}
-
-fn truncate(s: String, width: u16) -> String {
-    s.chars().take(width.into()).collect()
-}
-
-fn move_cursor(state: &mut State, i: i8, width: u16) {
-    if i > 0 {
-        state.cursor_position =
-            (state.cursor_position + u16::try_from(i).expect("Unreachable")).clamp(0, width);
-    } else {
-        state.cursor_position = match state
-            .cursor_position
-            .checked_sub(u16::try_from(i.abs()).expect("Unreachable"))
-        {
-            Some(x) => x,
-            None => 0,
+    fn prompt(
+        init_input: Option<Self>,
+        init_role: Role,
+        init_status: Status,
+        init_config: Config,
+    ) -> Result<Self>;
+    fn get_length(state: &State<Self>) -> usize;
+    fn move_cursor(state: &mut State<Self>, i: i8) {
+        if i > 0 {
+            state.cursor_position = (state.cursor_position
+                + usize::try_from(i).expect("Unreachable"))
+            .clamp(0, Self::get_length(state));
+        } else {
+            state.cursor_position = match state
+                .cursor_position
+                .checked_sub(usize::try_from(i.abs()).expect("Unreachable"))
+            {
+                Some(x) => x,
+                None => 0,
+            }
         }
     }
 }
 
+fn get_input_prefix(s: &String, i: usize) -> String {
+    return s.chars().take(i.into()).collect();
+}
+fn get_input_suffix(s: &String, i: usize) -> String {
+    return s.chars().skip(i.into()).collect();
+}
+
+fn insert_char_into_input(state: &mut State<String>, c: char) {
+    match state.input.clone() {
+        Some(input) => {
+            let mut result = String::from("");
+            result.push_str(&get_input_prefix(&input, state.cursor_position));
+            result.push_str(&c.to_string());
+            result.push_str(&get_input_suffix(&input, state.cursor_position));
+            state.input = Some(result);
+            // state.input.clear();
+            // state.input.push_str(result.as_str())
+            // s.clear();
+            // s.push_str(result.as_str());
+        }
+        None => {
+            state.input = Some(c.to_string());
+        }
+    }
+}
+
+fn remove_char_from_input(state: &mut State<String>) {
+    if let Some(input) = state.input.clone() {
+        let mut result = String::from("");
+        result.push_str(&get_input_prefix(
+            &input,
+            match state.cursor_position.checked_sub(1) {
+                Some(x) => x,
+                None => 0,
+            },
+        ));
+        result.push_str(&get_input_suffix(&input, state.cursor_position));
+        state.input = match result.is_empty() {
+            false => Some(result),
+            true => None,
+        };
+        // s.clear();
+        // s.push_str(result.as_str());
+    }
+}
+
+fn truncate(s: String, width: usize) -> String {
+    // println!("truncating {}", s);
+    s.chars().take(width.into()).collect()
+}
+
 impl Promptable for String {
     fn render_prompt(
-        result: &mut Self,
         role: &Role,
         status: &Status,
         config: &Config,
         event: Option<event::KeyEvent>,
-        state: &mut State,
+        state: &mut State<Self>,
     ) -> Result<bool> {
-        let width = 30;
         match event {
             Some(event) => match event.code {
                 event::KeyCode::Char(c) => {
-                    insert_char_into_input(result, state.cursor_position, c);
-                    move_cursor(state, 1, width);
+                    insert_char_into_input(state, c);
+                    Self::move_cursor(state, 1);
                 }
                 event::KeyCode::Backspace => {
-                    remove_char_from_input(result, state.cursor_position);
-                    move_cursor(state, -1, width);
+                    remove_char_from_input(state);
+                    Self::move_cursor(state, -1);
                 }
                 event::KeyCode::Left => {
-                    move_cursor(state, -1, width);
+                    Self::move_cursor(state, -1);
                 }
                 event::KeyCode::Right => {
-                    move_cursor(state, 1, width);
+                    Self::move_cursor(state, 1);
                 }
                 event::KeyCode::Enter => return Ok(true),
                 _ => (),
             },
             None => (),
         };
-        let result_len = u16::try_from(UnicodeWidthStr::width(result.as_str()))?;
+        // dbg!(&state);
+        // let result_len = u16::try_from(UnicodeWidthStr::width(result.as_str()))?;
         let prompt_len = u16::try_from(UnicodeWidthStr::width(config.prompt_text.as_str()))?;
         let hint_len = u16::try_from(UnicodeWidthStr::width(config.prompt_hint.as_str()))?;
+        let width = get_width(config)? - usize::from(prompt_len);
         execute!(
             io::stdout(),
             // cursor::RestorePosition,
@@ -152,13 +187,17 @@ impl Promptable for String {
                 Role::Completed => style::SetForegroundColor(style::Color::Green),
                 Role::Aborted => style::SetForegroundColor(style::Color::Red),
             },
-            match result.is_empty() {
-                true => style::Print(truncate(config.prompt_hint.clone(), width)),
-                false => style::Print(truncate(result.to_string(), width)),
+            match state.input.clone() {
+                None => style::Print(truncate(config.prompt_hint.clone(), width)),
+                Some(s) => style::Print(truncate(s, width)),
             },
-            match result.is_empty() {
-                true => cursor::MoveToColumn(prompt_len),
-                false => cursor::MoveToColumn(prompt_len + state.cursor_position),
+            match state.input.clone() {
+                None => cursor::MoveToColumn(prompt_len),
+                Some(_) => cursor::MoveToColumn(
+                    prompt_len
+                        + u16::try_from(state.cursor_position)
+                            .expect("Cursor position exceeds maximum 16 bit unsigned int value")
+                ),
             },
             style::ResetColor
         )?;
@@ -166,29 +205,24 @@ impl Promptable for String {
     }
 
     fn prompt(
-        init_result: Self,
+        init_input: Option<Self>,
         init_role: Role,
         init_status: Status,
         init_config: Config,
     ) -> Result<Self> {
         crossterm::terminal::enable_raw_mode()?;
-        let mut result = init_result;
         let role = init_role;
         let status = init_status;
         let config = init_config;
-        let mut state = State { cursor_position: 0 };
-        Self::render_prompt(&mut result, &role, &status, &config, None, &mut state)?;
+        let mut state = State {
+            input: init_input,
+            cursor_position: 0,
+        };
+        Self::render_prompt(&role, &status, &config, None, &mut state)?;
         loop {
             match event::read()? {
                 event::Event::Key(key) => {
-                    if Self::render_prompt(
-                        &mut result,
-                        &role,
-                        &status,
-                        &config,
-                        Some(key),
-                        &mut state,
-                    )? {
+                    if Self::render_prompt(&role, &status, &config, Some(key), &mut state)? {
                         break;
                     }
                 }
@@ -197,7 +231,17 @@ impl Promptable for String {
         }
         crossterm::terminal::disable_raw_mode()?;
         execute!(io::stdout(), style::Print("\n"))?;
-        Ok(result)
+        match state.input {
+            Some(i) => Ok(i),
+            None => Err(anyhow::anyhow!("Prompt exited without input")),
+        }
+    }
+
+    fn get_length(state: &State<Self>) -> usize {
+        match &state.input {
+            Some(s) => unicode_width::UnicodeWidthStr::width(s.as_str()),
+            None => 0,
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use tellmewhy::Promptable;
 
 fn main() -> Result<()> {
     let name = String::prompt(
-        String::new(),
+        // Some(String::new()),
+        None,
         tellmewhy::Role::Active,
         tellmewhy::Status::Neutral,
         tellmewhy::Config {
             prompt_text: "Enter your name: ".into(),
             prompt_hint: "Firstname Lastname".into(),
+            max_width: Some(30),
         },
     )?;
     println!("Hello, {name}");
